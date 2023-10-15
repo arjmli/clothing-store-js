5696325fd5cf29f46b385c801a79721f52b72fb0
alireza_jamali@hotmail.com <alireza@Alirezas-MacBook-Air.local>
2023-10-15 18:39:57 +0330

    new commit

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 const url = 'https://fakestoreapi.com/products'
 
-var arr
-
 function request(url) {
     fetch(url)
     .then(response => response.json())
-    .then(data => {arr = data})
+    .then(data => result(data))
 
 }
 
 
 request(url)
 
-console.log(arr);
+
+function result(data) {
+    console.log(data[0]);
+    console.log(data.title);
+    const trendingCardImg = document.querySelector('.trending-card-img1')
+    trendingCardImg.setAttribute('src', data[0].image)
+    const trendingCardTitle = document.querySelector('.trending-card-title1')
+    trendingCardTitle.textContent = data[0].title
+}
